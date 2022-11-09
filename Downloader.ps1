475ae33df1f2e1798aef0184441609ae3b779b8c
OlaYZen <Olaisenvb@gmail.com>
2022-11-09 15:45:39 +0100

    Downloader

---
+++ Downloader.ps1
@@ -0,0 +1,2 @@
+cd "C:\Users\$env:UserName\AppData\Roaming\BetterDiscord\themes"
+iwr -Uri https://raw.githubusercontent.com/OlaYZen/BetterDiscordAddons/master/Rounded.theme.css -OutFile Rounded.theme.css
