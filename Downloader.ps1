badbb4dcf426d363a06efa91962835ec94d8cea5
OlaYZen <Olaisenvb@gmail.com>
2022-11-15 10:45:45 +0100

    delete cache

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,2 +1,5 @@
+Stop-Process -Name discord -Force
+Remove-Item "C:\Users\$env:UserName\AppData\Roaming\discord\Cache\*"
 cd "C:\Users\$env:UserName\AppData\Roaming\BetterDiscord\themes"
 iwr -Uri https://raw.githubusercontent.com/OlaYZen/BetterDiscordAddons/master/Rounded.theme.css -OutFile Rounded.theme.css
+Start-Process "C:\Users\$env:UserName\AppData\Local\Discord\app-1.0.9007\discord.exe" & exit
