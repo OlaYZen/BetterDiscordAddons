00906dcb8833ae252fad6b84785b1bf00fcd21fa
OlaYZen <Olaisenvb@gmail.com>
2022-11-16 11:38:53 +0100

    updated script

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,5 +1,9 @@
 Stop-Process -Name discord -Force
+Start-Sleep -Seconds 3
 Remove-Item "C:\Users\$env:UserName\AppData\Roaming\discord\Cache\*"
-cd "C:\Users\$env:UserName\AppData\Roaming\BetterDiscord\themes"
-iwr -Uri https://raw.githubusercontent.com/OlaYZen/BetterDiscordAddons/master/Rounded.theme.css -OutFile Rounded.theme.css
+Start-Sleep -Seconds 1
+Set-Location "C:\Users\$env:UserName\AppData\Roaming\BetterDiscord\themes"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/OlaYZen/BetterDiscordAddons/master/Rounded.theme.css -OutFile Rounded.theme.css
+Start-Sleep -Seconds 1
+clear-Host
 Start-Process "C:\Users\$env:UserName\AppData\Local\Discord\app-1.0.9007\discord.exe" & exit 
