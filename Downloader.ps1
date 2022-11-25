bd8dfe19a6d9f0ec74f18b652a722faf870090b9
OlaYZen <Olaisenvb@gmail.com>
2022-11-25 15:35:31 +0100

    Updated the Downloader

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,9 +1,12 @@
+Write-Host "Stopping Discord"
 Stop-Process -Name discord -Force
 Start-Sleep -Seconds 3
+Write-Host "Deleting Discord Cache"
 Remove-Item "C:\Users\$env:UserName\AppData\Roaming\discord\Cache\*"
 Start-Sleep -Seconds 1
+Write-Host "Downloading newest verison of the theme"
 Set-Location "C:\Users\$env:UserName\AppData\Roaming\BetterDiscord\themes"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/OlaYZen/BetterDiscordAddons/master/Rounded.theme.css -OutFile Rounded.theme.css
 Start-Sleep -Seconds 1
-clear-Host
+Write-Host "Launching Discord"
 Start-Process "C:\Users\$env:UserName\AppData\Local\Discord\app-1.0.9007\discord.exe" & exit 
